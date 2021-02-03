842933a83b5f24f8435c9da76d3467091b9c20f6
Alena Trushnikova <alena.yu.t@gmail.com>
2021-02-02 16:19:53 -0800

    tests pass, cardFront doesn't load poster

--- src/card-components/CardFront.js
+++ src/card-components/CardFront.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 
 export default class CardFront extends Component {
+  // url = (poster) => {
+  //   return `src/assets/poster-imgs/${poster}.png`
+  // }
 
   render() {
     return (
-      <div className="card-front" style={{backgroundImage: `url(${''})`}}>
+      <div className="card-front" style={{backgroundImage: `url(${this.props.poster})`}}>
       </div>
+
     )
   }
 }
--- src/card-components/MovieCard.js
+++ src/card-components/MovieCard.js
@@ -33,11 +33,20 @@ export default class MovieCard extends Component {
     return (
       <div className="movie-card">
         {/* which component should receive which props? */}
-        <CardFront />
-        <CardBack />
+
+        <CardBack title={this.props.title}
+                  IMDBRating={this.props.IMDBRating}
+                  genres={this.props.genres} />
+        <CardFront poster={this.props.poster} />
       </div>
     )
   }
 }
 
 // Don't forget your default props!
+MovieCard.defaultProps = {
+  title: 'Unknown',
+  IMDBRating: null,
+  genres: ['No Genre(s) Found'],
+  poster: 'default'
+}
